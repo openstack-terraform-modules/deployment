af8f583b954ea3f5b80e4f8baf40234221940456
tannevaled <tannevaled@users.noreply.github.com>
2024-10-14 17:14:03 +0200

    wip

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-#terraform {
-#  required_version = ">= 1.9.5"
-#  required_providers {
-#    openstack = {
-#      source  = "terraform-provider-openstack/openstack"
-#      version = "~> 1.53.0"
-#    }
-#  }
-#}
