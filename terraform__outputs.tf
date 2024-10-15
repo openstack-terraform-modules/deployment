e70439602cdcfc1350cc362e1f7f53e31fc9ff1e
tannevaled <tannevaled@users.noreply.github.com>
2024-10-15 10:22:45 +0200

    wip

--- terraform__outputs.tf
+++ terraform__outputs.tf
@@ -28,3 +28,24 @@ output file_path {
 output project_path {
   value = local.project_path
 }
+
+output values {
+  value = object({
+    uuid = lookup(
+      data.external.deployment.result,
+      "deployment_uuid",
+      "NOT FOUND ERROR"
+    )
+    id = lookup(
+      data.external.deployment.result,
+      "deployment_id",
+      "NOT FOUND ERROR"
+    )
+    tenant_name = lookup(
+      data.external.deployment.result,
+      "tenant_name",
+     "NOT FOUND ERROR"
+    )
+    file_path = local.deployment_file_path
+  })
+}
